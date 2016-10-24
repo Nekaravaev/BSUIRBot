6089177bf9799bf704e80ff78d8366d0b54377fb
Andrew <andrewnyash@gmail.com>
2016-10-24 21:48:49 +0300

    Cron ready

--- app/User.php
+++ app/User.php
@@ -3,13 +3,13 @@
  * Created by PhpStorm.
  * User: karavaev
  * Date: 21.10.15
- * Time: 10.47
+ * Time: 10.47.
  */
-
-class User {
+class User
+{
     public $folder = 'info';
 
-    public function  __construct($folder)
+    public function __construct($folder)
     {
         $this->folder = $folder;
     }
@@ -17,83 +17,95 @@ class User {
     public function getUsers($returnArray = false)
     {
         $usersList = file_get_contents($this->folder.'/users.json');
-        if ($usersList)
-        {
+        if ($usersList) {
             return json_decode($usersList, $returnArray);
-        } else
+        } else {
             return false;
+        }
     }
 
     public function getCurrentUser($userID)
     {
         $currentUser = false;
         $usersList = $this->getUsers();
-        if ($usersList)
-        {
-            foreach ($usersList->groups as $groups)
-            {
-                foreach ($groups as $user)
-                {
-                    if ($user->user_id == $userID && $user->group_id != 'temp')
-                    {
+        if ($usersList) {
+            foreach ($usersList->groups as $groups) {
+                foreach ($groups as $user) {
+                    if ($user->user_id == $userID && $user->group_id != 'temp') {
                         $currentUser = $user;
                         break 2;
                     }
                 }
             }
         }
+
         return $currentUser;
+    }
+
+    public function getCronUsers()
+    {
+        $usersList = $this->getUsers();
+        $cronUsers = array();
+
+        if ($usersList) {
+            foreach ($usersList->groups as $groups) {
+                foreach ($groups as $user) {
+                    if ($user->cron && $user->group_id != 'temp') {
+                        $cronUsers[] = $user;
+                    }
+                }
+            }
+        }
+
+        return $cronUsers;
     }
 
     public function getUsersCount()
     {
         $return = 0;
         $json = $this->getUsers();
-        if ($json)
-        {
+        if ($json) {
             $return = (int) $json->users;
         }
+
         return $return;
     }
 
     public function manageUser($id, $params)
     {
-       $user = $this->getCurrentUser($id);
-            if ($user)
-            {
-                $this->updateUser($user->user_id,$params);
-            }
-            else
-            {
-                $this->newUser($id, $params);
-            }
-       return true;
+        $user = $this->getCurrentUser($id);
+        if ($user) {
+            $this->updateUser($user->user_id, $params);
+        } else {
+            $this->newUser($id, $params);
+        }
+
+        return true;
     }
 
     public function getGroup($gid)
     {
         $groups = $this->getUsers();
-        if ( ($groups) && isset($groups->groups->{$gid}))
-        {
+        if (($groups) && isset($groups->groups->{$gid})) {
             $group = $groups->groups->{$gid};
         }
+
         return ($group) ? $group : false;
     }
 
     private function newUser($id, $params)
     {
         $users = $this->getUsers(true);
-        if ($users)
-        {
+        if ($users) {
             $newUser = array(
-                'user_id'      => $id,
-                'group_id'     => $params['gid'],
-                'username'     => $params['username'],
+                'user_id' => $id,
+                'group_id' => $params['gid'],
+                'username' => $params['username'],
                 'display_name' => $params['display_name'],
-                'status'       => $params['status'],
-                'cron'         => $params['cron']
+                'status' => $params['status'],
+                'cron' => $params['cron'],
             );
-            if (!$this->getGroup($params['gid'])){
+            if (!$this->getGroup($params['gid'])) {
                 $users['groups'][$params['gid']] = array();
             }
 
@@ -101,41 +113,40 @@ class User {
             $usersCount = (int) $users['users'];
             $users['users'] = $usersCount + 1;
 
-            $groups = fopen($this->folder."/users.json", "w");
+            $groups = fopen($this->folder.'/users.json', 'w');
             $result = fwrite($groups, json_encode($users, JSON_PRETTY_PRINT));
             fclose($groups);
             $return = $result;
-        }
-        else
+        } else {
             $return = false;
+        }
+
         return $return;
     }
 
-    private function updateUser($id, $params){
+    private function updateUser($id, $params)
+    {
         //$userID, $userName, $userDisplayName, $status, $cron
         $users = $this->getUsers(true);
         $group = $params['gid'];
-            foreach ($users['groups'] as $singleGroup => $variables)
-            {
-                if ($singleGroup == $group)
-                {
-                    foreach ($variables as $value)
-                    {
-                        if ($value['user_id'] == $id)
-                        {
-                            $value['group_id']      = $group;
-                            $value['username']      = $params['username'];
-                            $value['display_name']  = $params['display_name'];
-                            $value['status']        = $params['status'];
-                            $value['cron']          = $params['cron'];
-                            break;
-                        }
+        foreach ($users['groups'] as $singleGroup => $variables) {
+            if ($singleGroup == $group) {
+                foreach ($variables as $value) {
+                    if ($value['user_id'] == $id) {
+                        $value['group_id'] = $group;
+                        $value['username'] = $params['username'];
+                        $value['display_name'] = $params['display_name'];
+                        $value['status'] = $params['status'];
+                        $value['cron'] = $params['cron'];
+                        break;
                     }
                 }
             }
-        $groups = fopen($this->folder."/users.json", "w");
+        }
+        $groups = fopen($this->folder.'/users.json', 'w');
         $result = fwrite($groups, json_encode($users, JSON_PRETTY_PRINT));
         fclose($groups);
+
         return $result;
     }
 }
--- helpers/update.php
+++ helpers/update.php
@@ -3,21 +3,39 @@
  * Created by PhpStorm.
  * User: karavaev
  * Date: 12.9.15
- * Time: 1.02
+ * Time: 1.02.
  */
+require_once '../autoload.php';
 
-class Cron {
-    function __construct(){
-     // return $this->updateGroups();
+class Cron
+{
+    public function __construct()
+    {
+        return $this->updateGroups();
     }
 
-    protected function updateGroups() {
+    protected function updateGroups()
+    {
         $xml = simplexml_load_file('http://www.bsuir.by/schedule/rest/studentGroup');
-        $groups = fopen("../info/groups.json", "w");
+        $groups = fopen('../info/groups.json', 'w');
         $result = fwrite($groups, json_encode($xml));
         fclose($groups);
+
         return $result;
     }
 }
 
 new Cron();
+
+$userClass = new User('info');
+$bot = new Bot('128735339:AAH1WyvktGZayrLDJe-SdeulXxGEEQaxN8M');
+$schedule = new BSUIR();
+
+$cronUsers = $userClass->getCronUsers();
+$date = $schedule->getDate();
+
+foreach ($cronUsers as $user) {
+    $msg = 'Доброе утро, '.$user->{'display_name'}.PHP_EOL.
+         'Сегодня твои занятия:'.PHP_EOL.$schedule->parseSchedule($schedule->getGroupSchedule($user->{'group_id'}, $date['day'], $date['week']));
+    $bot->sendMessage($user->{'user_id'}, $msg);
+}
