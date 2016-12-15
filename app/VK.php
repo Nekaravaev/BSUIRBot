754991e88131a66c78d2617ff859942df7cf4dda
Andrew <andrewnyash@gmail.com>
2016-12-15 16:40:58 +0300

    VK callback types :zap:

--- app/VK.php
+++ app/VK.php
@@ -1,52 +1,87 @@
 <?php
-	/**
-	 * Created by PhpStorm.
-	 * User: karavaev
-	 * Date: 28.11.16
-	 * Time: 5:47 PM
-	 */
+    /**
+     * Created by PhpStorm.
+     * User: karavaev
+     * Date: 28.11.16
+     * Time: 5:47 PM
+     */
 
 namespace bsuir\app;
+
 use bsuir\app\Bot;
 
 class VK extends Bot
 {
+    public function __construct($token)
+    {
+        parent::__construct($token);
+    }
 
-	public function __construct($token)
-	{
-		parent::__construct($token);
-	}
+    public function returnMessageInfo($message, $type)
+    {
+        if ($type != 'wall_post_new') {
+            $userFirstName = $this->getFirstname($message->object->user_id);
+        } else {
+            $userFirstName = ($message->object->from_id > 0) ? $this->getFirstname($message->object->user_id) : 'группы';
+        }
 
-	public function returnMessageInfo($message)
-	{
-		if (!empty($message->object->id)) {
-			$userInfo = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$message->object->user_id}&v=5.0"));
+        $return = [];
+        switch ($type) {
+            case 'message_new':
+                $return = [
+                    $message->object->user_id,
+                    $userFirstName,
+                    $message->object->title,
+                    $message->object->body,
+                    $message->type,
+                    $message
+                ];
+                break;
+            case 'message_allow':
+                $return = [
+                    $message->object->user_id,
+                    $userFirstName,
+                    $message
+                ];
+                break;
+            case 'message_deny':
+                $return = [
+                    $message->object->user_id,
+                    $userFirstName,
+                    $message
+                ];
+                break;
+            case 'wall_post_new':
+                $return = [
+                    $message->object->id,
+                    $message->group_id,
+                    $message->object->from_id,
+                    $message->object->owner_id,
+                    $userFirstName,
+                    $message
+                ];
+                break;
+        }
 
-			$userFirstName = $userInfo->response[0]->first_name;
+        return $return;
+    }
 
-			$return = [
-				$message->object->user_id,
-				$userFirstName,
-				$message->object->title,
-				$message->object->body,
-				$message->type,
-				$message
-			];
-		} else {
-			$return = false;
-		}
+    public function getFirstname($userId)
+    {
+        $userInfo = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$userId}&v=5.0"));
+        $userFirstName = $userInfo->response[0]->first_name;
 
-		return $return;
-	}
+        return ($userFirstName) ? $userFirstName : null;
+    }
 
-	public function sendMessage($chat, $reply)
-	{
-		$res = [
-			'message' => $reply,
-			'user_id' => $chat,
-			'access_token' => $this->token,
-			'v' => '5.0'
-		];
-		return $this->sendRequest("VK", ['method' => 'messages.send', 'params' => http_build_query($res), 'token' => $this->token]);
-	}
+    public function sendMessage($chat, $reply)
+    {
+        $res = [
+            'message' => $reply,
+            'user_id' => $chat,
+            'access_token' => $this->token,
+            'v' => '5.0'
+        ];
+        return $this->sendRequest("VK", ['method' => 'messages.send', 'params' => http_build_query($res), 'token' => $this->token]);
+    }
 }
--- vk.php
+++ vk.php
@@ -1,66 +1,109 @@
 <?php
-	/**
-	 * Created by PhpStorm.
-	 * User: Karavaev
-	 * Date: 7/25/2015
-	 * Time: 4:16 PM
-	 */
-	date_default_timezone_set("Europe/Minsk");
-	require __DIR__ . '/vendor/autoload.php';
-	use bsuir\app\VK as Bot;
-	use bsuir\app\BSUIR;
-	use bsuir\drivers\Redis as User;
-	use bsuir\helpers\Phrases;
+    /**
+     * Created by PhpStorm.
+     * User: Karavaev
+     * Date: 7/25/2015
+     * Time: 4:16 PM
+     */
+    error_reporting(0);
+    date_default_timezone_set("Europe/Minsk");
+    require __DIR__ . '/vendor/autoload.php';
+    use bsuir\app\VK as Bot;
+    use bsuir\app\BSUIR;
+    use bsuir\drivers\Redis as User;
+    use bsuir\helpers\Phrases;
 
+    header("HTTP/1.1 200 OK");
+    echo "ok";
 // init
-	$config   = json_decode(file_get_contents('info/config.json'));
-	$token    = $config->vk->token;
+    $config   = json_decode(file_get_contents('info/config.json'));
+    $token    = $config->vk->token;
 
-	$bot      = new Bot($token);
+    $bot      = new Bot($token);
+    $request = json_decode(file_get_contents('php://input'));
 
-	list( $chat, $name, $title, $message, $messageType, $message_raw ) = $bot->returnMessageInfo( json_decode( file_get_contents( 'php://input' ) ) );
 
-	$user  = new User('info');
-	$schedule = new BSUIR();
-	$phrase = new Phrases();
+    $user  = new User('info');
+    $schedule = new BSUIR();
+    $phrase = new Phrases();
 
-	$reply = $phrase::getPhrase('command404');
-	$groupId = $schedule->getGroupID("581062");
+    $reply = $phrase::getPhrase('command404');
+    $bsuirGroupId = $schedule->getGroupID("581062");
 
 //act by message
+if ($request->type == 'message_new') {
+    list($chat, $name, $title, $message, $messageType, $message_raw) = $bot->returnMessageInfo($request, $request->type);
 
-	if ($message == '/today') {
-		$date = $schedule->getDate();
-		$reply = $schedule->parseSchedule($schedule->getGroupSchedule($groupId, $date['day'], $date['week']));
-	}
+    if ($message == '/today') {
+        $date = $schedule->getDate();
+        $reply = $schedule->parseSchedule($schedule->getGroupSchedule($bsuirGroupId, $date['day'], $date['week']));
+    }
 
-	if ($message == '/date') {
-		$date = $schedule->getDate();
-		$reply = "Сегодня ".$date['day']." день".PHP_EOL.$date['week']." недели".PHP_EOL;
-	}
+    if ($message == '/date') {
+        $date = $schedule->getDate();
+        $reply = "Сегодня " . $date['day'] . " день" . PHP_EOL . $date['week'] . " недели" . PHP_EOL;
+    }
 
-	if ($message == '/tomorrow') {
-		$date = $schedule->getDate(true);
-		$reply = $schedule->parseSchedule($schedule->getGroupSchedule($groupId, $date['day'], $date['week']));
-	}
+    if ($message == '/tomorrow') {
+        $date = $schedule->getDate(true);
+        $reply = $schedule->parseSchedule($schedule->getGroupSchedule($bsuirGroupId, $date['day'], $date['week']));
+    }
 
-	if ($message == '/get') {
-		$reply = $phrase::getPhrase('get404');
-	}
+    if ($message == '/get') {
+        $reply = $phrase::getPhrase('get404');
+    }
 
-	if (preg_match('/^\/get [1-7] [1-4]/', $message)) {
-		$day  = substr($message, 5, 1);
-		$week = substr($message, 7, 1);
-		$reply = $schedule->parseSchedule($schedule->getGroupSchedule($groupId, $day, $week));
-	}
+    if (preg_match('/^\/get [1-7] [1-4]/', $message)) {
+        $day = substr($message, 5, 1);
+        $week = substr($message, 7, 1);
+        $reply = $schedule->parseSchedule($schedule->getGroupSchedule($bsuirGroupId, $day, $week));
+    }
 
-	if ($message == '/about') {
-		$reply = 'Запилил Андрей М. (@Karavay)';
-	}
+    if ($message == '/about') {
+        $reply = 'Запилил Андрей М. (@Karavay)';
+    }
 
-// end act by message
+    $bot->sendMessage($chat, $reply);
+    // end act by message
+}
 
-	$bot->sendMessage($chat, $reply);
+if ($request->type == 'message_allow') {
+    list($chat, $name, $message_raw) = $bot->returnMessageInfo($request, $request->type);
 
-	//return ok to callback API
-	echo "ok";
+    $usersArray = json_decode(file_get_contents('info/updateUsers.json'), true);
+
+    array_push($usersArray, $chat);
+    $updateUsers = fopen('info/updateUsers.json', 'w');
+    $result = fwrite($updateUsers, json_encode($usersArray, JSON_PRETTY_PRINT));
+    fclose($updateUsers);
+
+    $reply = $name. ', ты подписан на обновления стены.';
+    $bot->sendMessage($chat, $reply);
+}
+
+if ($request->type == 'message_deny') {
+    list($chat, $name, $message_raw) = $bot->returnMessageInfo($request, $request->type);
+
+    $usersArray = json_decode(file_get_contents('info/updateUsers.json'), true);
+
+    if (($key = array_search($chat, $usersArray)) !== false) {
+        unset($usersArray[$key]);
+    }
+
+    $updateUsers = fopen('info/updateUsers.json', 'w');
+    $result = fwrite($updateUsers, json_encode($usersArray, JSON_PRETTY_PRINT));
+    fclose($updateUsers);
+
+    $reply = $name. ', ты выпилен из списка получения обновлений стены.';
+
+    $bot->sendMessage($chat, $reply);
+}
+
+if ($request->type == 'wall_post_new') {
+    list($postId, $groupId, $fromId, $ownerId, $message_raw) = $bot->returnMessageInfo($request, $request->type);
+    $usersArray = json_decode(file_get_contents('info/updateUsers.json'), true);
+
+    $reply = json_encode($usersArray);
+
+    $bot->sendMessage(19857747, $reply);
+}
