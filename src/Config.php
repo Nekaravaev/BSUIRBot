bb05b4e55b07344a47f659bf77da2b07a7e7392d
Andrew <andrewnyash@gmail.com>
2017-10-05 10:48:08 +0300

    Fix getters in config

--- src/Config.php
+++ src/Config.php
@@ -33,36 +33,36 @@ class Config
 
     /**
      * @return string token
-     * @throws \Exception if Telegram Debug token not filled
+     * @throws BreakException if Telegram Debug token not filled
      */
     public function getTGDebugToken(): string
     {
-        if (self::$TGDebugToken)
-            return self::$TGDebugToken;
+        if ($this->TGDebugToken)
+            return $this->TGDebugToken;
         else
             throw new BreakException('No Telegram Debug token found');
     }
 
     /**
      * @return string token
-     * @throws \Exception if Confirmation token not filled
+     * @throws BreakException if Confirmation token not filled
      */
     public function getConfirmationCode(): string
     {
-        if (self::$VKConfirmationCode)
-            return self::$VKConfirmationCode;
+        if ($this->VKConfirmationCode)
+            return $this->VKConfirmationCode;
         else
             throw new BreakException('No VK confirmation code found');
     }
 
     /**
      * @return string token
-     * @throws \Exception if Telegram token not filled
+     * @throws BreakException if Telegram token not filled
      */
     public function getTGtoken(): string
     {
-        if (self::$TGtoken)
-            return self::$TGtoken;
+        if ($this->TGtoken)
+            return $this->TGtoken;
         else
             throw new BreakException('No Telegram token found');
     }
@@ -70,24 +70,24 @@ class Config
 
     /**
      * @return string token
-     * @throws \Exception if VK token not filled
+     * @throws BreakException if VK token not filled
      */
     public function getVKtoken():string
     {
-        if (self::$VKtoken)
-            return self::$VKtoken;
+        if ($this->VKtoken)
+            return $this->VKtoken;
         else
             throw new BreakException('No VK Token found');
     }
 
     /**
      * @return string token
-     * @throws \Exception if VK Debug token not filled
+     * @throws BreakException if VK Debug token not filled
      */
     public function getVKDebugToken():string
     {
-        if (self::$VKDebugToken)
-            return self::$VKDebugToken;
+        if ($this->VKDebugToken)
+            return $this->VKDebugToken;
         else
             throw new BreakException('No VK Debug token found');
     }
